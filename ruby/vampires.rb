36dbd3af39aa934427588fbe1fce0cf32ffdd40d
Jenna Van Conett <jenvanc@gmail.com>
2016-08-04 21:40:59 -0400

    Complete Release 1

---
+++ ruby/vampires.rb
@@ -0,0 +1,12 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you? What year were you born?"
+age = gets.chomp
+year_born = gets.chomp
+
+puts "Our company cadeteria serves gerlic bread. Should we order some for you?"
+garlic = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+insurance = gets.chomp
