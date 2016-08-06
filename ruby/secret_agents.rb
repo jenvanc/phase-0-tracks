3ac3bdd44affbe2bd41f05ba6a0eb86040a5617b
Jenna Van Conett <jenvanc@gmail.com>
2016-08-06 19:00:40 -0400

    Add pseudocode for algorithms

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,9 @@
+#encrypt algorithm
+#start at index [0]
+# -at current index, change letter to next letter in alphabet
+# -if a space character is present, leave space
+# -if next letter would be aa, change to a
+
+#decrypt algorithm
+#defining "abcdefghijklmnopqrstuvwxyz" as variable
+#use negative indexing to move back one more index to the previous letter
