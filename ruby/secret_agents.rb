84db71d5c475b2d57d28d6604aa8b99b3f0d31de
Jenna Van Conett <jenvanc@gmail.com>
2016-08-07 13:19:39 -0400

    Add encrypt method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -2,8 +2,21 @@
 #start at index [0]
 # -at current index, change letter to next letter in alphabet
 # -if a space character is present, leave space
-# -if next letter would be aa, change to a
 
 #decrypt algorithm
 #defining "abcdefghijklmnopqrstuvwxyz" as variable
-#use negative indexing to move back one more index to the previous letter
+# -if space in password, leave it be
+# -find index of letter in alphabet
+# -Once target index found, move back one index
+# -save new index as password to decrypt
+
+def encrypt(password)
+	count = 0
+	while count < password.length
+		if password[count] != " "
+			password[count] = password[count].next
+		end
+		count += 1
+	end
+	password
+end
