fb338c7f81655183dbede6245d4fc90915a2213c
Jenna Van Conett <jenvanc@gmail.com>
2016-08-16 20:32:12 -0400

    Add methods to Puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,29 @@ class Puppy
     toy
   end
 
+  def speak(integer)
+    "Woof!" * integer
+  end
+
+  def roll_over
+    "*rolls over*"
+  end
+
+  def dog_years(integer)
+    dog_age = integer * 7
+    dog_age
+  end
+
+  def swim
+    "*splash*"
+  end
+
 end
 
+Mango = Puppy.new
+
+p Mango.fetch("ball")
+p Mango.speak(4)
+p Mango.roll_over
+p Mango.dog_years(3)
+p Mango.swim
