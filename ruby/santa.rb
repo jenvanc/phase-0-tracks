e054a3292dd610274a45937ab32f3fd9ff6e3ff3
Jenna Van Conett <jenvanc@gmail.com>
2016-08-17 18:17:34 -0400

    Add Santa class and methods

---
+++ ruby/santa.rb
@@ -0,0 +1,19 @@
+class Santa
+  def speak
+    puts "Ho, ho, ho! Haaaappy holidays!"
+  end
+
+  def eat_milk_and_cookies(cookie)
+    puts "That was a good #{cookie}."
+  end
+
+  def initialize
+    puts "Initializing Santa instance..."
+  end
+
+end
+
+kringle = Santa.new
+
+kringle.speak
+kringle.eat_milk_and_cookies("lemon crinkle")
