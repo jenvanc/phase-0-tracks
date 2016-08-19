7a6315cd5d219234ef81016cfb31d632ca273f83
Jenna Van Conett <jenvanc@gmail.com>
2016-08-18 23:21:29 -0400

    Add methods for anger and change gender

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -7,13 +7,66 @@ class Santa
     puts "That was a good #{cookie}."
   end
 
-  def initialize
+  def initialize(gender, ethnicity)
     puts "Initializing Santa instance..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = [
+      "Rudolph", "Dasher", "Dancer",
+      "Prancer", "Vixen", "Comet",
+      "Cupid", "Donner", "Blitzen"
+    ]
+    @age = 0
   end
 
+  def celebrate_birthday
+    @age += 1
+  end
+
+  def get_mad_at(reindeer)
+    placement = @reindeer_ranking.index(reindeer)
+    @reindeer_ranking.insert(-1, @reindeer_ranking.delete_at(placement))
+  end
+
+  def gender=(new_gender)
+    @gender = new_gender
+  end
+
+  def age
+    @age
+  end
+
+  def ethnicity
+    @ethnicity
+  end
 end
 
-kringle = Santa.new
+santas = []
+
+genders = [
+  "agender", "female", "bigender",
+  "male", "female", "gender fluid",
+  ]
+
+ethnicities = [
+  "black", "Latino", "white",
+  "Japanese-African", "Mystical Creature (unicorn)", "N/A",
+  ]
+
+8.times do
+  santas << Santa.new(genders[rand(3)], ethnicities[rand(2)])
+end
+
+p santas
+
+kringle = Santa.new("male", "white")
 
 kringle.speak
 kringle.eat_milk_and_cookies("lemon crinkle")
+kringle.get_mad_at("Dasher")
+kringle.gender = "agender"
+kringle.celebrate_birthday
+
+p kringle
+p kringle.age
+p kringle.ethnicity
