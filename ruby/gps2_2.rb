e7e9cfa05caea1faab0fa3a4bf91170c7196d285
Jenna Van Conett <jenvanc@gmail.com>
2016-08-20 01:26:25 -0400

    Add methods for editing grocery list

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -4,62 +4,51 @@
   # use .split to seperate string items,
   # set default quantity
   # print the list to the console [can you use one of your other methods here?]
-# output: [what data type goes here, array or hash?]
+# output: hash
 
-#create the hash with items added to it
-def create_list(items)
-	list = {}
-	items = items.split
-	items.each do |i|
-		list[i] = 1
+def create_list(shopping_cart)
+	shopping_cart = shopping_cart.split
+	grocery_list = {}
+	shopping_cart.each do |item|
+		add_or_update_item(grocery_list, item)
 	end
-	p list
+	pretty_list(grocery_list)
 end
 
 # Method to add an item to a list
 # input: item name and optional quantity
-# steps: (item, quantity)
+# steps: (list, item, quantity)
 # output: the hash with new items added
 
-def add_item(list, item, quantity = 1)
+def add_or_update_item(list, item, quantity=1)
 	list[item] = quantity
-	p list
 end
 
 # Method to remove an item from the list
-# input: item
+# input: list, item
 # steps: take item as argument for deleting item
 # output: new hash
 
-def delete_item(item, list)
+def remove_item(list, item)
 	list.delete(item)
-	p list
 end
-
-# Method to update the quantity of an item
-# input: item, new quantity
-# steps:
-# output: new hash with updated quantity
-
-def update_quantity(item, new_quantity, list)
-	list[item] = new_quantity
-	p list
-end
-
-#iterate through hash contents and print items and quantities
 
 # Method to print a list and make it look pretty
 # input: list
-# steps:
-# output: string of list items in sentence
-def print_list(list)
-	list.each do |key, value|
-		p "There are #{value} #{key}"
+# steps: print item and quantity by iterating through hash
+# output: hash of list items in sentence
+
+def pretty_list(list)
+	list.each do |item, quantity|
+		puts "There are #{quantity} #{item} on the grocery list."
 	end
 end
 
-list = create_list("carrots apples cereal pizza bananas icecream")
-add_item(list, 'peaches', 3)
-delete_item('carrots', list)
-update_quantity('peaches', 5, list)
-print_list(list)
+list = create_list("lemonade tomatoes onions icecream")
+add_or_update_item(list, 'lemonade', 2)
+add_or_update_item(list, 'tomatoes', 3)
+add_or_update_item(list, 'icecream', 4)
+pretty_list(list)
+remove_item(list, 'lemonade')
+add_or_update_item(list, 'icecream', 1)
+pretty_list(list)
