cbfd3a7537fe0582cc79014916a04242d699b877
Jenna Van Conett <jenvanc@gmail.com>
2016-08-20 22:11:03 -0400

    Add module to mix in and new classes to call

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,38 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+#
+#   def self.yelling_happily(words)
+#     words + ". Yay!" + " :)"
+#   end
+# end
+#
+# p Shout.yell_angrily("Unbelievable")
+# p Shout.yelling_happily("Unbelievable")
+
 module Shout
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
 
-  def self.yelling_happily(words)
+  def yelling_happily(words)
     words + ". Yay!" + " :)"
   end
 end
 
-p Shout.yell_angrily("Unbelievable")
-p Shout.yelling_happily("Unbelievable")
+class Parent
+  include Shout
+end
+
+class Child
+  include Shout
+end
+
+mom = Parent.new
+p mom.yell_angrily("Clean up your room")
+p mom.yelling_happily("You got an 'A'")
+
+daughter = Child.new
+p daughter.yell_angrily("I can't believe you grounded me")
+p daughter.yelling_happily("I got a date to Prom")
