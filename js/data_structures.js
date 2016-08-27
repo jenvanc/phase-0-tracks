24a854de9d6a2ac2aae6875bcd5ec2b0db6b4dd9
Jenna Van Conett <jenvanc@gmail.com>
2016-08-27 12:56:32 -0400

    Add class Car and functions

--- js/data_structures.js
+++ js/data_structures.js
@@ -17,3 +17,30 @@ for (var i = 0; i < nameArray.length; i++) {
 	horse[nameArray[i]] = colorArray[i];
 }
 console.log(horse)
+
+var car = {}
+
+function Car(year, color) {
+  console.log("Our new car:", this);
+
+  this.year = year;
+  this.color = color;
+
+  this.start = function() {console.log("Vroom!"); };
+
+  console.log("Car manufacturing complete");
+}
+
+console.log("Lets make a car...");
+
+var newCar = new Car(1986, "green");
+console.log(newCar);
+console.log("Start your engine!");
+newCar.start();
+
+console.log("Let's male a car...");
+
+var newCar = new Car(1992, "blue");
+console.log(newCar);
+console.log("Start your engine!");
+newCar.start();
