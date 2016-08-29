347141d030d89effcb976dea9bdc0a548871a1e9
Jenna Van Conett <jenvanc@gmail.com>
2016-08-29 08:05:37 -0400

    Add function for making and storing random array

--- js/algos.js
+++ js/algos.js
@@ -11,11 +11,11 @@ function longPhrase(phrase) {
       longestPhrase = phrase[i];
       }
     }
-      return longestPhrase;
+    return longestPhrase;
 }
 
 // create a function that takes two objects as arguments
-// if the two objects have at least one key-value match return true
+// if the two objects have at least one key-value match return true, used sample input
 // else return false
 
 function keyValueMatch(object1, object2) {
@@ -28,6 +28,32 @@ function keyValueMatch(object1, object2) {
   }
 }
 
+// create a variable of the alphabet
+// create string of random letters from alphabet
+//  - save output of random letter to be stored in an array
+// create function to store output of random letter generator
+// store strings of random letter in array
+
+
+function randomLetter() {
+  var alphabet = "abcdefghijklmnopqrstuvwxyz"
+  var length = Math.floor(Math.random() * (10)) + 1;
+  string = ""
+
+  for (var i = 0; i < length; i++) {
+    letterPicker = Math.floor(Math.random() * (26));
+    string += alphabet.charAt(letterPicker);
+  }
+  return string
+}
+
+function arrayStorage(lengthOfArray) {
+  stringArray = []
+  for (var i = 1; i <= lengthOfArray; i++) {
+    stringArray.push(randomLetter())
+  }
+  return stringArray
+}
 
 console.log(longPhrase(["sunglasses", "purse", "lipgloss"]))
 console.log(longPhrase(["long phrase","longest phrase","longer phrase"]))
@@ -37,3 +63,12 @@ var person1 = { name: "Steven", age: 54 }
 var person2 = { name: "Tamir", age: 54 }
 console.log(keyValueMatch(person2, person1))
 console.log(keyValueMatch(person1, horse))
+
+console.log(arrayStorage(5))
+console.log(arrayStorage(3))
+
+for (var i = 0; i < 10; i++) {
+  arrayLength = Math.floor(Math.random() * 9) + 1;
+  list = arrayStorage(arrayLength)
+  console.log(longPhrase(list))
+}
