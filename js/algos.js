acde8487dfdffdc1df9a2c70ab635ca9f866c3dc
Jenna Van Conett <jenvanc@gmail.com>
2016-08-28 21:41:53 -0400

    Add function to match key-value pairs

--- js/algos.js
+++ js/algos.js
@@ -14,5 +14,26 @@ function longPhrase(phrase) {
       return longestPhrase;
 }
 
+// create a function that takes two objects as arguments
+// if the two objects have at least one key-value match return true
+// else return false
+
+function keyValueMatch(object1, object2) {
+    if(object1.name === object2.name) {
+      return true;
+  } else if(object1.age === object2.age) {
+      return true;
+  } else {
+      return false;
+  }
+}
+
+
 console.log(longPhrase(["sunglasses", "purse", "lipgloss"]))
 console.log(longPhrase(["long phrase","longest phrase","longer phrase"]))
+
+var horse = { name: "Priscilla", age: 6 }
+var person1 = { name: "Steven", age: 54 }
+var person2 = { name: "Tamir", age: 54 }
+console.log(keyValueMatch(person2, person1))
+console.log(keyValueMatch(person1, horse))
