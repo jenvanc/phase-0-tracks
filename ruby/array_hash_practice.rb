032e6f73e3638c709d20cf282a693f81d8193659
Jenna Van Conett <jenvanc@gmail.com>
2016-09-01 14:42:45 -0400

    Practice working with hashes

---
+++ ruby/array_hash_practice.rb
@@ -0,0 +1,13 @@
+employee = {
+  name: "Jenna Van Conett",
+  address: "507 N Lansing",
+  email: "jenvanc@gmail.com",
+  phone: "9895556666",
+  fav_shade_blue: "turquoise"
+}
+
+p employee
+employee[:phone] = "9893273132"
+p employee
+
+p employee[:name] + employee[:address]
