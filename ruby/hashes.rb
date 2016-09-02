219a4b79a47bc007fdcea756e49977eaf3a35a90
Jenna Van Conett <jenvanc@gmail.com>
2016-09-02 16:47:00 -0400

    Fix typos and add readable hash

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -23,11 +23,31 @@ client_info[:name] = gets.chomp
 puts "What is the client's age?"
 client_info[:age] = gets.chomp
 
-puts "How many children do you have?"
+puts "How many children does your client have?"
 client_info[:children] = gets.chomp.to_i
 
 puts "What is your decor theme?"
 client_info[:theme] = gets.chomp
 
 puts "Do you have red carpet? Please respond yes or no."
-client_info[:carpet] = is_true gets.chomp
+client_info[:red_carpet] = is_true(gets.chomp)
+
+puts "Here is your client's information"
+  client_info.each do |prompt, response|
+    puts "#{prompt}: #{response}"
+  end
+
+puts "Would you like to update any of this information?
+      Please type the name of the category you wish to update.
+      If not, please type 'none'."
+input = gets.chomp
+
+unless input == "none"
+  puts "Please type in the new information."
+  client_info[input.to_sym] = gets.chomp
+end
+
+puts "Here is your client's information"
+  client_info.each do |prompt, response|
+    puts "#{prompt}: #{response}"
+  end
