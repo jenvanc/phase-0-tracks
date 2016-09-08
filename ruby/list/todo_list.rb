d043302266d96c609434036320cad1382da10408
Jenna Van Conett <jenvanc@gmail.com>
2016-09-08 15:49:32 -0400

    Write methods for tests to pass

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,23 +1,25 @@
 class TodoList
-  def initialize(array)
-    @array = array
-  end
 
-  def get_items
-    @array
-  end
+   def initialize(list)
+   @list = list
+   end
 
-  def add_item(item)
-    @array << item
-  end
+   def get_items
+   @list
+   end
 
-  def delete_item(item)
-    @array.delete(item)
-  end
+   def add_item(item)
+   @list << item
+   end
 
-  def get_item(index)
-    @array[index]
-  end
+   def delete_item(item)
+   @list.delete(item)
+   end
+
+   def get_item(index)
+   @list[index]
+   end
 end
+
 
 # list = TodoList.new
