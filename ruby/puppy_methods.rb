504db1cd567dedef5432ebef5482211bcf09b41a
Jenna Van Conett <jenvanc@gmail.com>
2016-09-08 22:13:22 -0400

    Add puppy class methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,68 +1,30 @@
 class Puppy
-
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
 
-  def speak(integer)
-    "Woof!" * integer
+  def speak(int)
+  	puts "Woof!" * int
   end
 
   def roll_over
-    "*rolls over*"
+  	puts "*rolls over*"
   end
 
-  def dog_years(integer)
-    dog_age = integer * 7
-    dog_age
-  end
-
-  def swim
-    "*splash*"
+  def dog_years(int)
+  	dog_years = int * 7
+  	puts dog_years
   end
 
   def initialize
-    puts "Initializing new puppy instance..."
+  	puts "Initializing new puppy instance..."
   end
 end
 
-mango = Puppy.new
+luna = Puppy.new
 
-p mango.fetch("ball")
-p mango.speak(4)
-p mango.roll_over
-p mango.dog_years(3)
-p mango.swim
-
-class Pokemon
-  def initialize
-    puts "Initializing new Pokemon..."
-  end
-
-  def evolve
-    "Evolved!"
-  end
-
-  def heal(integer)
-    "Healed by #{integer} points!"
-  end
-end
-
-squirtle = Pokemon.new
-p squirtle.heal(15)
-p squirtle.evolve
-
-count = 0
-pokedex = []
-
-while count < 50
-  pokemon = Pokemon.new
-  pokedex << pokemon
-  count += 1
-end
-
-pokedex.each do |pokemon|
-  p pokemon.evolve
-  p pokemon.heal(5)
-end
+luna.fetch("ball")
+luna.speak(2)
+luna.roll_over
+luna.dog_years(5)
