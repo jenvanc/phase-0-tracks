32190d05503b00a560ced06a5555a299c8b9f960
Jenna Van Conett <jenvanc@gmail.com>
2016-09-14 16:10:31 -0400

    More tests for reversing a string

--- js/explore.js
+++ js/explore.js
@@ -1,5 +1,5 @@
 // Create empty string variable reversedString
-// Find last letter of string
+// Find last letter of string passed to function
 // Add last letter to new variable, continue to add new last letter to variable
 // return reversedString
 
@@ -12,9 +12,10 @@ function reverse(string) {
 }
 
 var practiceTest = reverse("hello")
+var anotherTest = reverse("waterbottle")
 
 console.log(practiceTest)
 
-if (1 == 1) {
-  console.log(practiceTest)
+if (anotherTest === "elttobretaw") {
+  console.log(anotherTest)
 }
