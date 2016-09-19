b118edac47f25b78dc265cc3516a4d6b9d2de845
Jenna Van Conett <jenvanc@gmail.com>
2016-09-19 01:16:59 -0400

    Add method for key value matching and generating words

--- js/algos.js
+++ js/algos.js
@@ -19,13 +19,14 @@ function longPhrase(phrase) {
 // else return false
 
 function keyValueMatch(object1, object2) {
-    if(object1.name === object2.name) {
-      return true;
-  } else if(object1.age === object2.age) {
-      return true;
-  } else {
-      return false;
+  for (var i in object1) {
+    if (object2.hasOwnProperty(i)) {
+      if (object1[i] === object2[i]) {
+        return true;
+      }
+    }
   }
+  return false;
 }
 
 // create a variable of the alphabet
@@ -34,25 +35,21 @@ function keyValueMatch(object1, object2) {
 // create function to store output of random letter generator
 // store strings of random letter in array
 
-
 function randomLetter() {
-  var alphabet = "abcdefghijklmnopqrstuvwxyz"
-  var length = Math.floor(Math.random() * (10)) + 1;
-  string = ""
-
-  for (var i = 0; i < length; i++) {
-    letterPicker = Math.floor(Math.random() * (26));
-    string += alphabet.charAt(letterPicker);
-  }
-  return string
+  return 'abcdefghijklmnopqrstuvwxyz'.charAt(Math.floor(Math.random() * 26));
 }
 
-function arrayStorage(lengthOfArray) {
-  stringArray = []
-  for (var i = 1; i <= lengthOfArray; i++) {
-    stringArray.push(randomLetter())
+function generateRandomWords(n) {
+  var results = [];
+  for (var i = 0; i < n; i++) {
+    var length = Math.floor(Math.random() * 10 + 1);
+    var word = "";
+    for (var j = 0; j < length; j++) {
+      word += randomLetter();
+    }
+    results.push(word);
   }
-  return stringArray
+  return results;
 }
 
 console.log(longPhrase(["sunglasses", "purse", "lipgloss"]))
@@ -61,14 +58,11 @@ console.log(longPhrase(["long phrase","longest phrase","longer phrase"]))
 var horse = { name: "Priscilla", age: 6 }
 var person1 = { name: "Steven", age: 54 }
 var person2 = { name: "Tamir", age: 54 }
-console.log(keyValueMatch(person2, person1))
-console.log(keyValueMatch(person1, horse))
-
-console.log(arrayStorage(5))
-console.log(arrayStorage(3))
+console.log(keyValueMatch(person2, person1));
+console.log(keyValueMatch(person1, horse));
 
 for (var i = 0; i < 10; i++) {
-  arrayLength = Math.floor(Math.random() * 9) + 1;
-  list = arrayStorage(arrayLength)
-  console.log(longPhrase(list))
+  var wordArray = generateRandomWords(5);
+  console.log(wordArray);
+  console.log(longPhrase(wordArray));
 }
