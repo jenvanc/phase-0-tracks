5d312a7a938c84db8b5db83c4de208633c0636c8
Jenna Van Conett <jenvanc@gmail.com>
2016-09-26 00:11:19 -0400

    Register student

--- databases/db_project/prof_and_students.rb
+++ databases/db_project/prof_and_students.rb
@@ -3,6 +3,7 @@ require 'faker'
 
 db = SQLite3::Database.new("semester.db")
 
+
 create_prof_table = <<-SQL
   CREATE TABLE IF NOT EXISTS professors (
     id INTEGER PRIMARY KEY,
@@ -51,7 +52,34 @@ def create_course(db, course_num, subject)
   db.execute("INSERT INTO classes (course_num, subject) VALUES (?, ?)", [course_num, subject])
 end
 
-10.times do
-  n = rand(100..999)
-  create_course(db, n, subjects.sample)
+# 10.times do
+#   n = rand(100..999)
+#   create_course(db, n, subjects.sample)
+# end
+
+def create_prof(db, name)
+  db.execute("INSERT INTO professors (name) VALUES (?)", [name])
 end
+
+# 5.times do
+#   create_prof(db, Faker::Name.name)
+# end
+
+def register_student(db, name, age)
+  db.execute("INSERT INTO students (name, age) VALUES (?, ?)", [name, age])
+end
+
+def get_student_id(db, name, age)
+  db.execute("SELECT id FROM students WHERE name=? AND age=?", [name, age])
+end
+
+puts "Welcome to the Enroll-o-matic at Get-Smart University!"
+puts "What is your name?"
+name = gets.chomp
+puts "Whats is your age?"
+age = gets.chomp.to_i
+
+# register_student(db, name, age)
+student_id = get_student_id(db, name, age).first
+
+puts "Your student id is #{student_id}"
