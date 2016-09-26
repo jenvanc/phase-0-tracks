fcf3ab4073887b5e49f7ff8f97cafa921ddd1c57
Jenna Van Conett <jenvanc@gmail.com>
2016-09-26 02:37:57 -0400

    Print enrollment

--- databases/db_project/prof_and_students.rb
+++ databases/db_project/prof_and_students.rb
@@ -37,6 +37,10 @@ subjects = ["psychology", "computer science", "mathematics", "anatomy",
   "biology", "chemsitry", "philosophy", "underwater basketweaving", "fencing",
   "street fighting", "professional gaming", "business"]
 
+def enroll_student(db, class_id, student_id)
+  db.execute("INSERT INTO enrollment (class_id, student_id) VALUES (?, ?)", [class_id, student_id])
+end
+
 def create_course(db, course_num, subject)
   db.execute("INSERT INTO classes (course_num, subject) VALUES (?, ?)", [course_num, subject])
 end
@@ -53,23 +57,33 @@ def get_student_id(db, name, age)
   db.execute("SELECT * FROM students WHERE name=? AND age=?", [name, age])
 end
 
+def show_enrollments(db, student_id)
+  statement = <<-SQL
+    SELECT *
+    FROM   enrollment JOIN classes ON classes.id = enrollment.class_id
+    WHERE  enrollment.student_id = ?
+  SQL
+  db.execute(statement, student_id)
+end
+
 def class_options(db, student_id)
   statement = <<-SQL
-    SELECT  classes.id,
+    SELECT DISTINCT
+            classes.id,
             classes.subject,
             classes.course_num
     FROM    students,
-            classes LEFT OUTER JOIN enrollment ON students.id != enrollment.student_id
-    WHERE   students.id =?
+            classes LEFT OUTER JOIN enrollment ON students.id = enrollment.student_id
+    WHERE   students.id = ?
   SQL
 
   db.execute(statement, student_id)
 end
 
 # create course
-10.times do
-  create_course(db, rand(100..999), subjects.sample)
-end
+# 10.times do
+#   create_course(db, rand(100..999), subjects.sample)
+# end
 
 puts "Welcome to the Enroll-o-matic at Get-Smart University!"
 puts "What is your name?"
@@ -87,5 +101,18 @@ classes = class_options(db, student["id"])
 
 classes.each_index do |i|
   c = classes[i]
-  puts "#{i}. #{c["subject"].upcase} #{c["course_num"]}"
+  puts "#{i + 1}. #{c["subject"].upcase} #{c["course_num"]}"
+end
+
+puts "Please select a class by number to enroll, type 'quit' to exit"
+enroll = gets.chomp
+until enroll == "quit"
+  enroll_student(db, enroll.to_i, student["id"])
+  enroll = gets.chomp
+end
+
+puts "You are enrolled in"
+enrollments = show_enrollments(db, student["id"])
+enrollments.each do |enrollment|
+  puts "#{enrollment["subject"].upcase} #{enrollment["course_num"]}"
 end
